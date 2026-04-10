2f942cb4b1d384f3ab63c6521dfe2cc511f5ac84
Uchenna Adubasim <uchadubs@yahoo.com>
2026-04-10 18:23:03 +0100

    Improved logging and other updates

--- helpers.py
+++ helpers.py
@@ -4,7 +4,19 @@ import logging
 from pygame import mixer
 
 
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(name=__name__)
+logger.setLevel(level=logging.INFO)
+
+console_handler = logging.StreamHandler()
+file_handler = logging.FileHandler(filename='logs.log', mode='a')
+console_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.INFO)
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -21,5 +33,5 @@ def space_audio():
     mixer.init()
     mixer.music.load(bg_audio)
     mixer.music.play(loops=-1)  # Play the music (-1 means loop forever)
-    logging.info(f"Background music playing {bg_audio}")
+    logger.info(f"Background music playing {bg_audio}")
 
--- main.py
+++ main.py
@@ -3,11 +3,32 @@ import random
 import time
 import logging
 import winsound
+import sys
 from helpers import resource_path, space_audio
 from stars import draw_left_stars, draw_right_stars
+from tkinter import *
+from tkinter import messagebox
 
 
-logging.basicConfig(level=logging.INFO)
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+#Create handlers; set console handler level to debug and file handler to warning
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('logs.log', mode='a')
+file_handler.setLevel(logging.INFO)
+# create formatter
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to console and file handlers
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+# add console and file handlers to logger
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 # screen
 screen = turtle.Screen()
@@ -22,27 +43,26 @@ sun_img = resource_path(r'assets\images\sun.gif')
 moon_img = resource_path(r'assets\images\crescent_moon.png')
 rocket_img = resource_path(r'assets\images\placidplace-rocket-12320_512.gif')
 screen.bgpic(bg_img)
-logging.info(f"Background image added {bg_img}")
+logger.info(f"Background image added {bg_img}")
 screen.register_shape(name='sun', shape=sun_img)
 screen.register_shape(name='moon', shape=moon_img)
 screen.register_shape(name='rocket', shape=rocket_img)
 screen.colormode(255)
-
 # pen
 pen = turtle.Turtle()
+pen.ht()
 pen.pencolor('yellow')
 pen.pensize(2)
 pen.speed(0)
-pen.ht()
 # rocket
 rocket = turtle.Turtle()
 # font
 font_name = 'Pacifico'
 font_size = 50
 font_type = 'italic'
-
+# initialise variables
 correct_answer = 0
-suns = moons = 0   # initialise variables
+suns = moons = skipped = 0   
 
 
 def move_pen(x, y):
@@ -58,44 +78,51 @@ def setUp():
     pen.write(title, align='center', font=(font_name, font_size+20, font_type))
     # Draw stars
     screen.tracer(0)    # turn off animation so stars appear at once instead of drawing each star 
-    logging.info('Animation turned off')
+    logger.info('Animation turned off')
     draw_left_stars()
     draw_right_stars()
+    logger.info("Stars drawn")
     screen.tracer(1) # turn on animation
-    logging.info('Animation turned back on')
+    logger.info('Animation turned back on')
 
 
-# keep this here so it doesn't repeat in the loop for every question ##########
+###### keep this here so it doesn't repeat in the loop for every question ######
 setUp()
-game_mode = screen.numinput(
-    "Choose Level of Difficulty",
-    "Enter '1' for easy, '2' for medium, '3' for hard or '4' for pro")
+game_mode_mapping = {'1': 'Easy', '2': 'Medium', '3': 'Hard', '4': 'Pro'} 
+game_mode = screen.textinput(
+    "Level of Difficulty",
+    """Enter '1' for easy, '2' for medium, '3' for hard, '4' for pro 
+    or any other number to end the game""")
 ###############################################################################
     
 def check_game_mode():
     """Difficulty level of the game"""
     min = max = 0
-    
-    match int(game_mode):
-        case 1:
-            min, max = 0, 9
-        case 2:
-            min, max = 10, 99
-        case 3:
-            min, max = 100, 999
-        case 4:
-            min, max = 1000, 9999
-        case _:
-            logging.error("""We didn't understand what you entered. 
-                             You can only choose levels 1-4""")
-    logging.info(
-        "Level %d set with min value %d and max value %d" % (
-            int(game_mode), 
-            min, 
-            max
-            )
-        )
-    return min, max
+    if not game_mode.isdigit():
+        messagebox.showerror('Invalid Entry', 
+                             'Level of Difficulty should be a number from 1-4')
+        logger.error(
+            '''Input '%s' not accepted. 
+            You can only enter an number from 1-4. 
+            Exiting Game...''' % game_mode)
+        sys.exit()
+    else:
+        match game_mode:
+            case '1':
+                min, max = 0, 9
+            case '2':
+                min, max = 10, 99
+            case '3':
+                min, max = 100, 500
+            case '4':
+                min, max = 500, 999
+            case _:
+               logger.warning('Exiting Game...')
+               sys.exit()
+    logger.info(
+        '''Level: %s, Min value: %s, Max value: %s
+        ''' % (game_mode_mapping[game_mode], min, max))
+    return min, max    
 
 def operation():
     """Return the chosen math operation"""
@@ -104,6 +131,7 @@ def operation():
     number_1 = random.randint(min, max)
     operator = random.choice(operators)
     number_2 = random.randint(1, max)
+    logger.info("Question: %d %s %d" % (number_1, operator, number_2))
     return number_1, operator, number_2, operators
 
 def rocket_func():
@@ -136,15 +164,21 @@ def countdown():
 def show_message():
     """Display the title message"""
     msg = "Get a Sun or a Moon"
+    hints = ('Hint (Addition): 2 + 10 = 12', 
+             'Hint (Subtraction): 9 - 7 = 2', 
+             'Hint (Multiplication): 6 * 10 = 60', 
+             'Hint (Division): 20 / 5 = 4', 
+             'Power: 3 ^ 3 = 3*3*3 = 27')
     move_pen(0, 400)
     for _ in range(1):
         r = random.randint(200, 255)
         g = random.randint(200, 255)
         b = random.randint(200, 255)
         pen.pencolor(r, g, b)
-        pen.write(msg, align='center', font=('Arial', 12, 'italic'))
-        time.sleep(2)
-        pen.clear()
+        pen.write(msg + '\n' + random.choice(hints), 
+                  align='center', font=('Arial', 12, 'italic'))
+        #time.sleep(5)
+        #pen.clear()
 
 def question_and_answer():
     """Return correct answer and user answer"""
@@ -158,14 +192,18 @@ def question_and_answer():
     pen.write(number_2, align='center', font=(font_name, font_size, font_type))
     move_pen(200, 100)
     pen.write("=", align='center', font=(font_name, font_size, font_type))
-    print('Question:', number_1, operator, number_2)
-    user_answer = screen.numinput(
+    
+    user_answer = screen.textinput(
         "Answer", 
         """
         Enter the correct answer 
         (Approximate division questions to 2 decimal places): 
         """
         )
+    try:
+        user_answer = float(user_answer)
+    except Exception as exc:
+        logger.error("An error occured: %s" % exc)
     if operator == '+':
         correct_answer = number_1 + number_2
     elif operator == '-':
@@ -176,16 +214,14 @@ def question_and_answer():
         correct_answer = round((number_1 / number_2), 2)
     elif operator == '^':
         correct_answer = number_1 ** number_2
-    else:
-        pass
     return correct_answer, user_answer
 
 def mark_answer(): 
     """Mark answer""" 
     correct_answer, user_answer = question_and_answer()
-    global suns, moons
+    global suns, moons, skipped
     move_pen(0, -200)
-    if user_answer is not None:
+    if user_answer is not None and isinstance(user_answer, float):
         if user_answer == correct_answer:
             right_turtle = turtle.Turtle()
             right_turtle.shape('sun')
@@ -215,22 +251,29 @@ def mark_answer():
                 time.sleep(0.1)
                 wrong_turtle.ht()
             moons += 1
-    logging.info(f'correct answer {correct_answer}')
-    logging.info(f'user answer {user_answer}')
-    return suns, moons
+    else:
+        skipped += 1
+    logger.info(f'correct answer {correct_answer}')
+    logger.info(f'user answer {user_answer}')
+    return suns, moons, skipped
 
 def analytics():
-    move_pen(-200, -400)
+    move_pen(-300, -400)
     pen.pencolor('yellow')
     pen.write(
         f"{suns} ☀️", align='center', font=('Comic Sans MS', 30, 'bold')
         )
-    move_pen(0, -400)
+    move_pen(-100, -400)
     pen.pencolor('white')
     pen.write(
         f"{moons} 🌙", 
         align='center', font=('Comic Sans MS', 30, 'bold'))
-    move_pen(200, -400)
+    move_pen(100, -400)
+    pen.pencolor('gray')
+    pen.write(
+        f"{skipped} skipped", 
+        align='center', font=('Comic Sans MS', 30, 'italic'))
+    move_pen(300, -400)
     pen.pencolor('orange')
     pen.write(
         f"{int(no_of_tries)-int(no_of_questions)} left", 
@@ -241,35 +284,37 @@ def game_over_func():
     move_pen(0, 0)
     pen.pencolor('red')
     end_msg = 'Game Over'
-    logging.info(end_msg)
+    logger.info(end_msg)
     pen.write(end_msg, align='center', font=(font_name, font_size, font_type))
 
 
 if __name__ == '__main__':
     game_over = False
     no_of_questions = 0
-    no_of_tries = screen.numinput(
+    no_of_tries = screen.textinput(
         "Number of questions", 
-        "Enter the number of questions you would like to answer")
-    #exit_duration = 20
-    countdown()
-    rocket_func()
-    while not game_over and no_of_questions < int(no_of_tries):
-        show_message()
-        suns, moons = mark_answer()
-        no_of_questions += 1
-        pen.clear()
-        analytics()
+        """Enter the number of questions you would like to answer 
+        or any other character to exit""")
+    if no_of_tries is None or not no_of_tries.isdigit():
+        logger.warning('Exiting Game...')
+        sys.exit()
     else:
-        pen.clear()
-        game_over_func()
-    
-    analytics()
-    # loggings
-    logging.info(f'suns {suns}')
-    logging.info(f'moons {moons}')
-    logging.info("Total number of questions: %s" % no_of_questions)
+        countdown()
+        rocket_func()
+        while not game_over and no_of_questions < int(no_of_tries):
+            show_message()
+            suns, moons, skipped = mark_answer()
+            no_of_questions += 1
+            pen.clear()
+            analytics()
+        else:
+            pen.clear()
+            game_over_func()
+        
+        analytics()
+        # loggings
+        logger.info(f'suns {suns}')
+        logger.info(f'moons {moons}')
+        logger.info(f'Skipped {skipped}')
+        logger.info("Total number of questions: %s" % no_of_questions)
     screen.mainloop()
-
-
-
